5accdb5307e4aebbbc779d4e2883b15e3b09f215
Sergey Ivanov <ivanovsa69@yandex.ru>
2023-11-13 21:57:57 +0300

    final commit_1

--- src/test/java/ru/netology/services/FreelanceServiceTest.java
+++ src/test/java/ru/netology/services/FreelanceServiceTest.java
@@ -1,4 +1,24 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FreelanceServiceTest {
-  
+    @Test
+    public void testCalculate3months() {
+        FreelanceService service = new FreelanceService();
+        int actual = service.calculate(10000, 3000, 20000);
+        int expected = 3;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCalculate2months() {
+        FreelanceService service = new FreelanceService();
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
+        Assertions.assertEquals(expected, actual);
+    }
 }
